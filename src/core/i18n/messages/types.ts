ad70e66ef4207400a440eaac9afef9c1056d31cb
iuliandita <iulian.dita@gmail.com>
2026-04-11 15:25:02 +0200

    fix(i18n): restore strict locale catalogs

--- src/core/i18n/messages/types.ts
+++ src/core/i18n/messages/types.ts
@@ -1,6 +1,6 @@
 import { en } from './en'
 
 export type MessageKey = keyof typeof en
-export type MessageCatalog = Partial<Record<MessageKey, string>>
+export type MessageCatalog = { [Key in MessageKey]: string }
 
 export const MESSAGE_KEYS = Object.freeze(Object.keys(en) as MessageKey[])
